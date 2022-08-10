181c3f1e9043ac56b94f00cf295322b583a1994e
Yulia <yulia1201556@gmail.com>
2022-08-11 02:03:19 +0300

    Max из трёх

---
+++ task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 4. 
+//Напишите программу, которая принимает 
+// на вход три числа и выдаёт максимальное
+//  из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if ((a > b) && (a > c))
+{
+    Console.Write($"Максимальное число {a} ");
+}
+else if ((b > a) && (b > c))
+{
+    Console.Write($"Максимальное число {b} ");
+}
+else if ((c > a) && (c > b))
+{
+    Console.Write($"Максимальное число {c} ");
+}
+else if (a == b)
+{
+    Console.Write($"Максимальное число {a} и {b} равны");
+}
+else if (a == c)
+{
+    Console.Write($"Максимальное число {a} и {c} равны");
+}
+else if (b == c)
+{
+    Console.Write($"Максимальное число {b} и {c} равны");
+}
+
